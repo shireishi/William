42342b0ce7dc1266c958fe8680246a25e46056fa
404 <63125641+shireishi@users.noreply.github.com>
2022-02-24 22:52:17 -0700

    Starting some commnd parsing

--- src/main.c
+++ src/main.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-int main(void) {
-    printf("Hello world\n");
+const char* commands[] = {"test"};
+
+bool args_parser(int argc, const char** argv) {
+    bool is_valid_command;
+
+    int lenocomlist = sizeof(commands) / sizeof(const char*);
+    printf("%d\n", lenocomlist);
+
+    for (int i = 0; i < lenocomlist; i++) {
+        for (int j = 0; j < argc; j++) {
+            if (commands[i] == argv[j]) {
+                is_valid_command = true;
+            }
+        }
+    }
+    if (is_valid_command != true) {
+        is_valid_command = false;
+    }
+
+    return is_valid_command;
+}
+
+int main(int argc, const char** argv) {
+    for (int i = 0; i < argc; i++) {
+        printf("%s\n", argv[i]);
+    }
+    printf("\n");
+    bool command = args_parser(argc, argv);
+    if (command == true) {
+        printf("valid command\n");
+    } else if (command == false) {
+        printf("invalid command\n");
+    } else {
+        printf("an error has occurred\n");
+    }
+
     return 0;
 }
