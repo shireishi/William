df636d14eb15cb4d36f4fc4fc027b1af6a79bfb6
El <63125641+shireishi@users.noreply.github.com>
2022-03-02 10:14:05 -0700

    Adding some more system support

--- src/system.c
+++ src/system.c
@@ -1,10 +1,18 @@
-const char* colored(int r, int g, int b, const char* text) {
+const char* colored(int r, int g, int b, const char* title, const char* text) {
     const char* color_string[1024];
-    sprintf(color_string, "\033[38;2;%d;%d;%dm%s\033[m", r, g, b, text);
+    sprintf(color_string, "[\033[38;2;%d;%d;%dm%s\033[m] : %s", r, g, b, title, text);
     printf("%s\n", color_string);
     return color_string;
 }
 
 void notification(char* text) {
-    printf(colored(0, 125, 0, text));
+    printf(colored(0, 125, 0, "Notif", text)); // prints a notification
+}
+
+void error(char* text) {
+    printf(colored(255, 0, 0, "Error", text)); // prints an error message
+}
+
+void broadcast(char* text) {
+    printf(colored(125, 125, 0, "Broad", text)); // prints a general message to the terminal
 }
