2346ba97e5f9a0e7fa68f6ce6c80195f7d8e108b
El <63125641+shireishi@users.noreply.github.com>
2022-03-02 09:52:57 -0700

    Modifying some syntax and adding a new command

--- src/main.c
+++ src/main.c
@@ -1,18 +1,18 @@
+// SYSTEM IMPORTS
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
 
+// LOCAL IMPORTS
 #include "system.c"
 
+// GLOBAL DEFINITIONS
 #define debug 0
 
-const char* commands[] = {"test"};
+// GLOBAL VARIABLES
+const char* commands[] = {"test", "echo"};
 
-/*
-Figure out how to make lencomliststr work with the concatination 
-with lencomlistint
-*/
-
+// COMMAND STRUCT
 typedef struct {
     const char* command_text;
     int verified;
@@ -21,6 +21,10 @@ typedef struct {
 // COMMANDS
 void test_command() {
     printf("Hello world\n");
+}
+
+void echo(const char* text) {
+    printf(text);
 }
 
 // ARGUMENT PARSER
@@ -62,6 +66,7 @@ Command args_parser(int argc, const char** argv) {
 int main(int argc, const char** argv) {
     // variable declaration
     Command command_struct = args_parser(argc, argv);
+    args_length = (sizeof(argv)/sizeof(const char*));
     int command = command_struct.verified;
     const char* command_text = command_struct.command_text;
 
@@ -74,17 +79,29 @@ int main(int argc, const char** argv) {
     switch (command) { // determine if the arguments provided are commands
         case true:
             notification("valid command\n");
+            
+            char* arguments[(sizeof(argv)/sizeof(const char*)-2)]; // set the size of the arguments list to the remaining arguments in the array of arguments provided to the command
+            if (args_length > 2)
+            for (int i = 2; i < argc-2; i++) {
+                arguments[i-2] = argv[i];
+            }
+            
             break;
         case false:
-            printf("invalid command\n");
+            printf("Not a valid command\n");
             break;
         default:
-            printf("an error has occurred\n");
+            printf("A fatal error has occured in the command parser.\n");
+            exit();
             break;
     }
 
     if (strcmp(command_text, commands[0]) == 0) { // determine the corresponding function for the command string
         test_command();
+    } else if (strcmp(command_text, commands[1]) == 0) {
+        echo(arguments);
+    } else {
+        exit();
     }
 
     return 0;
