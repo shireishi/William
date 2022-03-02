ecc229982a2d00a37e46322a6a4844a676a54e20
El <63125641+shireishi@users.noreply.github.com>
2022-03-02 07:51:26 -0700

    Fixed a sprintf statement

--- src/system.c
+++ src/system.c
@@ -1,5 +1,5 @@
 const char* colored(int r, int g, int b, const char* text) {
-    char* color_string = NULL;
+    const char* color_string[1024];
     sprintf(color_string, "\033[38;2;%d;%d;%dm%s\033[m", r, g, b, text);
     printf("%s\n", color_string);
     return color_string;
