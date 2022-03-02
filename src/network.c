2122ee43cef8f7f43a5e40ffe588ef96a1654f9d
El <63125641+shireishi@users.noreply.github.com>
2022-03-02 10:55:31 -0700

    Taking notes on how to design the network system

--- src/network.c
+++ src/network.c
@@ -10,15 +10,26 @@
 #include <stdbool.h>
 
 #include "system.c"
+#include "security.c"
 
 #define ADDRESS "192.168.0.21"
 #define PORT 8080
 #define HEADER 64
 #define FORMAT "utf-8"
 
-// https://www.tutorialspoint.com/multithreading-in-c
-// https://www.educative.io/edpresso/how-to-implement-tcp-sockets-in-c
+/*
+multithreading in c
+https://www.tutorialspoint.com/multithreading-in-c
 
+socket programming in c
+https://www.educative.io/edpresso/how-to-implement-tcp-sockets-in-c
+
+look through the above links to combine and create a server using tcp socket connections that is also
+multithreaded and can handle asynchronous function calls
+*/
 void start_server() {
-    
+    /*
+    start the server and when a connection is accepted,
+    hash their ip and set that as the thread id upon the opening of the thread.
+    */
 }
